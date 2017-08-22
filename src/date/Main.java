9f1776ea4a798fd5e5b8af99e32cf6a20f6a25e4
Berkmans, Clement(cberkmans) <cberkmans@paypal.com>
2017-08-21 18:45:49 -0700

    date

--- src/date/Main.java
+++ src/date/Main.java
@@ -1,14 +1,37 @@
 package date;
 
 import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.TimeZone;
 
 public class Main {
 
 	public static void main(String a[]) throws Exception {
-		System.out.println(lastUpdatedOn("2017-05-20T00:16:13.000+0000"));
-		System.out.println(lastUpdatedOn("2017-05-22T00:16:13.000+0000"));
+		//System.out.println(lastUpdatedOn("2017-05-20T00:16:13.000+0000"));
+		//System.out.println(lastUpdatedOn("2017-05-22T00:16:13.000+0000"));
+		//dateToEpoch()
+		
+		System.out.println(new Date().getTime());
+		//System.out.println(Calendar.getInstance(TimeZone.setDefault("UTC");).);
+		System.out.println(Calendar.getInstance().getTimeInMillis());
+		System.out.println(new Date(new SimpleDateFormat("yyyy/MM/dd").format(new Date())).getTime());
+		
+		// Getting UTC time long value with 3 zero and converting into epoch value
+		SimpleDateFormat isoFormat = new SimpleDateFormat("yyyy/MM/dd");
+		isoFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
+		isoFormat.format(new Date());
+		long timeWithZero = isoFormat.parse(isoFormat.format(new Date())).getTime();
+		System.out.println(Long.toString(timeWithZero));
+		String timeWithZeroStr = Long.toString(timeWithZero);
+		System.out.println(Long.parseLong(timeWithZeroStr.substring(0, timeWithZeroStr.length()-3)));
+		
+		//System.out.println(isoFormat.getCalendar().getTimeInMillis());
+		//Date date = isoFormat.parse("2010-05-23T09:01:02");
+		
+		
 	}
 	private static String lastUpdatedOn(String dateTime) throws Exception {
 		DateFormat df = new SimpleDateFormat("yyy-MM-dd'T'HH:mm");
@@ -31,5 +54,13 @@ public class Main {
 		return dateTime;
 	}
 	
+	public void dateToEpoch(String strDate){	
+		//strDate = "04/28/2016";
+		try {
+		    Long millis = new SimpleDateFormat("MM/dd/yyyy").parse(strDate).getTime();
+		} catch (ParseException e) {
+		    e.printStackTrace();
+		}
+	}
 }
 
